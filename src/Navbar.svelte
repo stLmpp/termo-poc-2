9d878a5dca2d43c66bb9c15a068b0528d9e60d3f
stLmpp <gui.stlmpp@hotmail.com>
2022-03-02 00:37:04 -0300

    - fix container on navbar

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,5 +1,5 @@
 <nav class="navbar fixed-top navbar-light bg-primary navbar-dark">
-  <div class="container-fluid">
+  <div class="container">
     <span class="navbar-brand mb-0 h1">Termo predictions</span>
   </div>
 </nav>
