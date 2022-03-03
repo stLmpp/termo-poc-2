355bc5ee2886541e046e05c13fcde625db7d5cf1
stLmpp <gui.stlmpp@hotmail.com>
2022-03-03 00:27:46 -0300

    feat(Navbar): added github icon

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import { version } from '../package.json';
+  import Icon from './Icon.svelte';
 </script>
 
 <nav class="navbar fixed-top navbar-light bg-primary navbar-dark">
   <div class="container">
     <span class="navbar-brand mb-0 h1">Termo predictions v{version}</span>
+    <div class="d-flex">
+      <a class="btn btn-primary" target="_blank" href="https://github.com/stLmpp/termo-predictions">
+        <Icon icon="github" />
+      </a>
+    </div>
   </div>
 </nav>
