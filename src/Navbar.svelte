e4223bbee6a502ae6385adfa3979c24d63b600b5
stLmpp <gui.stlmpp@hotmail.com>
2022-03-03 00:07:35 -0300

    feat(Navbar): added version

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+  import { version } from '../package.json';
+</script>
+
 <nav class="navbar fixed-top navbar-light bg-primary navbar-dark">
   <div class="container">
-    <span class="navbar-brand mb-0 h1">Termo predictions</span>
+    <span class="navbar-brand mb-0 h1">Termo predictions v{version}</span>
   </div>
 </nav>
