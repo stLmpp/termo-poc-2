c23165e35e73e84f473536713f7fcb8dbe3fda97
stLmpp <gui.stlmpp@hotmail.com>
2022-03-06 18:47:31 -0300

    chore(): remove comment and update yarn

--- server/database.js
+++ server/database.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@termo-predictions.gdwqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function getClientConnection() {
