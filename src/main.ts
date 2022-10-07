dfc6352e6270897283f2272930eb2dd7ef48824c
stLmpp <gui.stlmpp@gmail.com>
2022-10-07 14:02:05 -0300

    - added firebase sdk

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,20 @@ import App from './App.svelte';
 const app = new App({ target: document.getElementById('app')! });
 
 export default app;
+
+import { initializeApp, type FirebaseOptions } from 'firebase/app';
+import { initializeAnalytics } from 'firebase/analytics';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyCAhcr6HJJ0uhdkbbSU6hCmG7CvOvlA0vQ',
+  authDomain: 'termo-predictions.firebaseapp.com',
+  projectId: 'termo-predictions',
+  storageBucket: 'termo-predictions.appspot.com',
+  messagingSenderId: '810053753067',
+  appId: '1:810053753067:web:00e272f82a53569baf06ae',
+  measurementId: 'G-H6YVPNP2N4',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+import.meta.env.PROD && initializeAnalytics(firebaseApp);
